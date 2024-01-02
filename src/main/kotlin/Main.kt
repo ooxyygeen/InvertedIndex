d009dac8d8f728c0ef978cd8fad58ced18c9216a
oxygen <ooxyygen0@gmail.com>
2024-01-03 00:46:38 +0200

    feat: add thread pool for managing connections and command-line argument handling

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,32 @@
 import java.net.ServerSocket
+import java.util.concurrent.Executors
+import java.util.concurrent.atomic.AtomicBoolean
 import kotlin.concurrent.thread
 
-fun main() {
+fun main(args: Array<String>) {
+    val threadNum = if (args.isNotEmpty()) {
+        try {
+            args[0].toInt()
+        } catch (e: NumberFormatException) {
+            1
+        }
+    } else {
+        1
+    }
     val serverSocket = ServerSocket(8080)
+    val index = InvertedIndex()
+    val files = getAllTextFiles("src/main/resources/dataset")
+    val isCompleted = AtomicBoolean(false)
+
+    thread(start = true) { Indexer(files, threadNum, isCompleted, index).run() }
+
+    val clientThreadPool = Executors.newCachedThreadPool()
 
     while (true) {
         val clientSocket = serverSocket.accept()
-        println("client accepted $clientSocket")
-        val client = ClientHandler(clientSocket)
-        thread {
+        println("New client accepted $clientSocket")
+        val client = ClientHandler(clientSocket, index, isCompleted)
+        clientThreadPool.execute {
             client.run()
         }
     }
